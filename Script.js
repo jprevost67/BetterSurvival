8bd6ef032672780d1da830be744d6f865a4dc301
jprevost67 <jprevostKHS67@gmail.com>
2016-05-24 08:48:48 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -81,42 +81,63 @@ var createBlock = function(id,name,texture,source,isOpaque,render,destroy,explos
     Block.setDestroyTime(id,destroy);
     Block.setExplosionResistance(id,explosion);
     Block.setLightLevel(id,light);
+    Block.setLightOpacity(id,0);
     if(addCreative == true){
         if(extraData >= 0){
             Player.addItemCreativeInv(id,1,0);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 1){
+            Player.addItemCreativeInv(id,1,1);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 2){
+            Player.addItemCreativeInv(id,1,2);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 3){
+            Player.addItemCreativeInv(id,1,3);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 4){
+            Player.addItemCreativeInv(id,1,4);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 5){
+            Player.addItemCreativeInv(id,1,5);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 6){
+            Player.addItemCreativeInv(id,1,6);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 7){
+            Player.addItemCreativeInv(id,1,7);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 8){
+            Player.addItemCreativeInv(id,1,8);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 9){
+            Player.addItemCreativeInv(id,1,9);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 10){
+            Player.addItemCreativeInv(id,1,10);
         }
-        if(extraData >= 0){
-            Player.addItemCreativeInv(id,1,0);
+        if(extraData >= 11){
+            Player.addItemCreativeInv(id,1,11);
         }
+        if(extraData >= 12){
+            Player.addItemCreativeInv(id,1,12);
+        }
+        if(extraData >= 13){
+            Player.addItemCreativeInv(id,1,13);
+        }
+        if(extraData >= 14){
+            Player.addItemCreativeInv(id,1,14);
+        }
+        if(extraData >= 15){
+            Player.addItemCreativeInv(id,1,15);
+        }
+    }
+};
+
+var createBlock = function(id,name,texture,source,stackSize,isTool,addCreative) {
+    ModPE.setItem(id,texture,source,name,stackSize);
+    Item.setHandEquipped(id,isTool);
+    if(addCreative == true){
+        Player.addItemCreativeInv(id,1,0);
     }
 };
