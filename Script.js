ff8cc3b36f6529006625c78659e5c8734eff4e37
jprevost67 <jprevostKHS67@gmail.com>
2016-05-24 08:24:14 -0400

    Began Adding IDs

---
+++ Script.js
@@ -0,0 +1,70 @@
+var Items = {
+    Crushed: {
+        Coal: 450,
+        Iron: 451,
+        Gold: 452,
+    },
+    Knives: {
+        Wood: 453,
+        Stone: 454,
+        Iron: 455,
+        Gold: 456,
+        Diamond: 457,
+    },
+    Hammers: {
+        Wood: 458,
+        Stone: 459,
+        Iron: 460,
+        Gold: 461,
+        Diamond: 462,
+    },
+    Gears: {
+        Wood: 463,
+        Stone: 464,
+        Iron: 465,
+    },
+    Wrenches: {
+        Normal: 466,
+    },
+    Buckets: {
+        Concrete: 467,
+    },
+};
+var Blocks = {
+    Logs: {
+        Apple: 210,
+        Orange: 211,
+    },
+    Leaves: {
+        Apple: 212,
+        Orange: 213,
+    },
+    Saplings: {
+        Apple: 214,
+        Orange: 215,
+    },
+    Planks: {
+        Apple: 216,
+        Orange: 217,
+    },
+    Slabs: {
+        Apple: 218,
+        Orange: 219,
+    },
+    Cobble_Compressed: {
+        Single: 220,
+        Double: 221,
+        Triple: 222,
+        Quadruple: 223,
+    },
+    Concrete: {
+        Wet: 224,
+        Dry: 225,
+    },
+    Ores: {
+        Tin: 226,
+    },
+    Machinery: {
+        Gearbox: 227,
+    },
+};
