2c8e5cb7e3ed2317e2cb76f2412be4571376aa40
jprevost67 <jprevostKHS67@gmail.com>
2016-05-24 11:39:36 -0400

    Update Barrel.js

--- Barrel.js
+++ Barrel.js
@@ -3,14 +3,16 @@ var BarrelFunction = function(x,y,z){
     this.y = y;
     this.z = z;
     this.id = 228;
+    this.fillId = 229;
     this.filledPercent = 0;
-    this.state = "Empty";
+    this.state = "empty";
     this.isRaining = false;
     this.isDestroyed = false;
     this.place = function(){
         if(this.isDestroyed == false){
             setTile(this.x,this.y,this.z,this.id);
         }
+        if(this.state == "empty")
     };
     this.getIsRaining = function(){
         
