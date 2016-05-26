b316e4e6a105b4c421d36e635ead6c248ead1a0d
jprevost67 <jprevostKHS67@gmail.com>
2016-05-26 09:53:08 -0400

    Added Barrel Function
    Moved barrel function to main code

--- Script.js
+++ Script.js
@@ -223,6 +223,66 @@ var Random = function(numbersCanChoseFrom){
     return r;
 };
 
+var BarrelFunction = function(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.id = 228;
+    this.fillId = 229;
+    this.filledPercent = 0;
+    this.state = "empty";
+    this.isRaining = false;
+    this.isDestroyed = false;
+    this.place = function() {
+        if(this.isDestroyed == false) {
+            setTile(this.x,this.y,this.z,this.id);
+            if(this.state == "water") {
+                setTile(x,y+1,z,this.fillId,0);
+                Block.setShape(229,2.5/16,0.125/16,2.5/16,13.5/16,this.filledPercent/100,13.5/16);
+            }
+        }
+    };
+    this.getIsRaining = function() {
+        if(Level.getRainLevel() > 0) {
+            this.isRaining = true;
+        }
+        else {
+            this.isRaining = false;
+        }
+    };
+    this.fillBarrel = function(placeWater) {
+        if(this.isRaining == true){
+            if(this.state == "empty" || this.state == "water"){
+                this.state = "water";
+                if(this.filledPercent < 100){
+                    this.filledPercent++;
+                }
+            }
+        }
+        if(placeWater == true){
+            if(this.state == "empty" || this.state == "water"){
+                this.state = "water";
+                this.filledPercent = 100;
+            }
+        }
+    };
+    this.emptyBarrel = function(){
+        this.state = "empty";
+        this.filledPercent = 0;
+        setTile(x,y+1,z,0);
+    }
+    this.useOn = function(x,y,z,i,b,s,id,bd) {
+        
+    };
+    this.destroy = function() {
+        this.isDestroyed = true;
+    };
+    this.run = function() {
+        this.place();
+        this.getIsRaining();
+    }
+};
+
 var registerItems = function(){
     var crushedCoal = new createItem(Items.Crushed.Coal,"Crushed Coal","crushed_coal",0,64,false,true);
     var crushedIron = new createItem(Items.Crushed.Iron,"Crushed Iron","crushed_iron",0,64,false,true);
@@ -297,5 +357,8 @@ var registerBlocks = function() {
     
     var woodBarrel = new createBlock(Blocks.Barrels.Normal,"Wooden Barrel",[["barrel",2],["barrel",1],["barrel",0],["barrel",0],["barrel",0],["barrel",0]],5,0,0,1,1,0,true,0);
     var barrelFill = new createBlock(Blocks.Barrels.Fill,"Barrel Fill",[["still_water",0]],5,0,0,1,1,0,false,0);
-    
 };
+
+registerItems();
+registerBlocks();
+
