26b25d486ab9ce9a9b687ca59998bdd8a193335c
ElectricGamer67 <jprevostKHS67@gmail.com>
2016-05-27 10:52:42 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,45 +1,53 @@
 var Items = {
     Crushed: {
-        Coal: 450,
-        Iron: 451,
-        Tin: 452,
-        Gold: 453,
+        Coal: 500,
+        Iron: 501,
+        Copper: 502,
+        Tin: 503,
+        Gold: 504,
     },
     Knives: {
-        Wood: 454,
-        Stone: 455,
-        Iron: 456,
-        Gold: 457,
-        Diamond: 458,
+        Wood: 510,
+        Stone: 511,
+        Iron: 512,
+        Copper: 513,
+        Tin: 514,
+        Gold: 515,
+        Diamond: 516,
     },
     Hammers: {
-        Wood: 459,
-        Stone: 460,
-        Iron: 461,
-        Gold: 462,
-        Diamond: 463,
+        Wood: 520,
+        Stone: 521,
+        Iron: 522,
+        Copper: 523,
+        Tin: 524,
+        Gold: 525,
+        Diamond: 526,
     },
     Gears: {
-        Wood: 464,
-        Stone: 465,
-        Iron: 466,
+        Wood: 530,
+        Stone: 531,
+        Iron: 532,
+        Tin: 533,
     },
     Wrenches: {
-        Normal: 467,
+        Normal: 540,
     },
     Buckets: {
-        Concrete: 468,
+        Concrete: 550,
     },
     Ingots: {
-        Tin: 469
+        Copper: 560,
+        Tin: 561,
     },
     Nuggets: {
-        Iron: 470,
-        Tin: 471
+        Iron: 570,
+        Copper: 571,
+        Tin: 572,
     },
     Misc: {
-        Rock: 472,
-        Silk_Strand: 473,
+        Rock: 580,
+        Silk_Strand: 581,
     },
 };
 var Blocks = {
@@ -74,14 +82,15 @@ var Blocks = {
         Dry: 225,
     },
     Ores: {
-        Tin: 226,
+        Copper: 226,
+        Tin: 227,
     },
     Machinery: {
-        Gearbox: 227,
+        Gearbox: 228,
     },
     Barrels: {
-        Normal: 228,
-        Fill: 229,
+        Normal: 229,
+        Fill: 230,
     },
 };
 
@@ -227,8 +236,8 @@ var BarrelFunction = function(x,y,z) {
     this.x = x;
     this.y = y;
     this.z = z;
-    this.id = 228;
-    this.fillId = 229;
+    this.id = 229;
+    this.fillId = 230;
     this.filledPercent = 0;
     this.state = "empty";
     this.isRaining = false;
@@ -238,7 +247,6 @@ var BarrelFunction = function(x,y,z) {
             setTile(this.x,this.y,this.z,this.id);
             if(this.state == "water") {
                 setTile(x,y+1,z,this.fillId,0);
-                Block.setShape(229,2.5/16,0.125/16,2.5/16,13.5/16,this.filledPercent/100,13.5/16);
             }
         }
     };
@@ -330,24 +338,30 @@ var generateOre = function(id,minY,maxY,rarity){
 var registerItems = function(){
     var crushedCoal = new createItem(Items.Crushed.Coal,"Crushed Coal","crushed_coal",0,64,false,true);
     var crushedIron = new createItem(Items.Crushed.Iron,"Crushed Iron","crushed_iron",0,64,false,true);
+    var crushedCopper = new createItem(Items.Crushed.Copper,"Crushed Copper","crushed_copper",0,64,false,true);
     var crushedTin = new createItem(Items.Crushed.Tin,"Crushed Tin","crushed_tin",0,64,false,true);
     var crushedGold = new createItem(Items.Crushed.Gold,"Crushed Gold","crushed_gold",0,64,false,true);
     
     var knifeWood = new createItem(Items.Knives.Wood,"Wooden Knife","knife_wood",0,1,true,true);
     var knifeStone = new createItem(Items.Knives.Stone,"Stone Knife","knife_stone",0,1,true,true);
     var knifeIron = new createItem(Items.Knives.Iron,"Iron Knife","knife_iron",0,1,true,true);
+    var knifeCopper = new createItem(Items.Knives.Copper,"Copper Knife","knife_copper",0,1,true,true);
+    var knifeTin = new createItem(Items.Knives.Tin,"Tin Knife","knife_tin",0,1,true,true);
     var knifeGold = new createItem(Items.Knives.Gold,"Gold Knife","knife_gold",0,1,true,true);
     var knifeDiamond = new createItem(Items.Knives.Diamond,"Diamond Knife","knife_diamond",0,1,true,true);
     
     var hammerWood = new createItem(Items.Hammers.Wood,"Wooden Hammer","hammer_wood",0,1,true,true);
     var hammerStone = new createItem(Items.Hammers.Stone,"Stone Hammer","hammer_stone",0,1,true,true);
     var hammerIron = new createItem(Items.Hammers.Iron,"Iron Hammer","hammer_iron",0,1,true,true);
+    var hammerCopper = new createItem(Items.Hammers.Copper,"Copper Hammer","hammer_copper",0,1,true,true);
+    var hammerTin = new createItem(Items.Hammers.Tin,"Tin Hammer","hammer_tin",0,1,true,true);
     var hammerGold = new createItem(Items.Hammers.Gold,"Gold Hammer","hammer_gold",0,1,true,true);
     var hammerDiamond = new createItem(Items.Hammers.Diamond,"Diamond Hammer","hammer_diamond",0,1,true,true);
     
     var gearWood = new createItem(Items.Gears.Wood,"Wooden Gear","gear_wood",0,64,true,true);
     var gearStone = new createItem(Items.Gears.Stone,"Stone Gear","gear_stone",0,64,true,true);
     var gearIron = new createItem(Items.Gears.Iron,"Iron Gear","gear_iron",0,64,true,true);
+    var gearTin = new createItem(Items.Gears.Iron,"Iron Gear","gear_iron",0,64,true,true);
     
     var wrench = new createItem(Items.Wrenches.Normal,"Wrench","wrench",0,1,true,true);
     
@@ -356,6 +370,7 @@ var registerItems = function(){
     var ingotTin = new createItem(Items.Ingots.Tin,"Tin Ingot","ingot_tin",0,64,false,true);
     
     var nuggetIron = new createItem(Items.Nuggets.Iron,"Iron Nugget","nugget_iron",0,64,false,true);
+    var nuggetCopper = new createItem(Items.Nuggets.Copper,"Copper Nugget","nugget_copper",0,64,false,true);
     var nuggetTin = new createItem(Items.Nuggets.Tin,"Tin Nugget","nugget_tin",0,64,false,true);
     
     var rock = new createItem(Items.Misc.Rock,"Rock","rock",0,64,false,true);
@@ -395,6 +410,7 @@ var registerBlocks = function() {
     var concreteWet = new createBlock(Blocks.Concrete.Wet,"Concrete (Liquid)",[["concrete",0]],1,0,0,9999,9999,0,false,0);
     var concreteDry = new createBlock(Blocks.Concrete.Dry,"Concrte",[["concrete",0]],1,0,0,1.5,2,0,true,0);
     
+    var copperOre = new createBlock(Blocks.Ore.Copper,"Copper Ore",[["ore_tin",0]],1,0,0,1.5,2,0,true,0);
     var tinOre = new createBlock(Blocks.Ore.Tin,"Tin Ore",[["ore_tin",0]],1,0,0,1.5,2,0,true,0);
     
     var gearbox = new createBlock(Blocks.Machinery.Gearbox,"Gearbox",[["gearbox",0]],5,0,0,1,1,0,true,0);
